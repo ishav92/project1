b294768f6b8c5d42c70dd760adf15a8d21ad8fdf
ishav92 <vermaisha2108@gmail.com>
2022-06-18 15:50:44 +0530

    add function module in python program

--- file.py
+++ file.py
@@ -1,9 +1,11 @@
-a=10
-b=20
-if (a>b):
-    print("a is greater")
-elif (a<b) && (a%2==0):
-    print("a is even")
-else:
-    print("b is greater")
-    print("hello isha , True")
+    
+def funtion():
+    a=10
+    b=20
+    if (a>b):
+        print(a)
+    else:
+        print(b)
+        print("hello isha , True")
+
+function()
